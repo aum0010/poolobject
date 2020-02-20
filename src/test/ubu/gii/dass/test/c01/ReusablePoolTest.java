0769e2bff32777978a80050619fd0768835262ee
Alberto <Alberto@DESKTOP-0VLA6VF>
2020-02-20 18:46:27 +0100

    Realizado metodo testReleaseReusable

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,6 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.DuplicatedInstanceException;
 import ubu.gii.dass.c01.NotFreeInstanceException;
 import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
@@ -67,10 +68,21 @@ public class ReusablePoolTest {
 
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * @throws NotFreeInstanceException 
+	 * @throws DuplicatedInstanceException 
 	 */
 	@Test
-	public void testReleaseReusable() {
-		fail("Not yet implemented");
+	public void testReleaseReusable() throws NotFreeInstanceException, DuplicatedInstanceException  {
+		Reusable testPool1 = pool.acquireReusable();
+		Reusable testPool2 = pool.acquireReusable();
+
+		pool.releaseReusable(testPool1);	
+		pool.releaseReusable(testPool2);	
+
+		assertEquals("Comprobamos que la ultima instancia creada es la que sera adquirida",testPool2, pool.acquireReusable());
+
+		assertNotEquals("Comprobamos que las instancias no son iguales", testPool1, testPool2);
+
 	}
 
 }
